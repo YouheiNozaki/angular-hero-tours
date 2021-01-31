8fd1489d9664a71b44015e0c28936e67acbc2af0
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-01-31 21:45:19 +0900

    display hero list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,14 +8,14 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  }
+
   heroes = HEROES;
-  constructor() { }
+  selectedHero: Hero | undefined;
+  constructor() {}
 
   ngOnInit(): void {
   }
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
+  }
 }
