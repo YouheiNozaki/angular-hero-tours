ea516539bfd7e68c0d7a04badd64ff9b9bc82347
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-02-02 01:04:20 +0900

    create link

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,16 +11,11 @@ import { MessageService } from "../message.service"
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero: Hero | undefined;
   heroes: Hero[] | undefined
   constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   ngOnInit(): void {
     this.getHeroes()
-  }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero
-    this.messageService.add(`HeroesComponent: Selected heroid=${hero.id}`)
   }
 
   getHeroes(): void {
