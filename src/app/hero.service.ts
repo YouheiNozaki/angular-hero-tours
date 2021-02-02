621f7184a9e5aaa999c7604cdedf4c695793b0e0
Youhei Nozaki <youhey.hey0505@gmail.com>
2021-02-02 22:49:56 +0900

    fix

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { MessageService } from "./message.service"
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 import { Hero } from "./hero"
 import { HEROES } from "./mock-heroes"
 
@@ -9,14 +12,49 @@ import { HEROES } from "./mock-heroes"
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
-  getHeroes(): Observable<Hero[]> {
-    this.messageService.add("HeroService: fetched heroes")
-    return of(HEROES)
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService,
+  ) { }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`)
   }
+  private heroesUrl = "api/heroes"
+
+  /**
+ * 失敗したHttp操作を処理します。
+ * アプリを持続させます。
+ * @param operation - 失敗した操作の名前
+ * @param result - observableな結果として返す任意の値
+ */
+private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: リモート上のロギング基盤にエラーを送信する
+    console.error(error); // かわりにconsoleに出力
+
+    // TODO: ユーザーへの開示のためにエラーの変換処理を改善する
+    this.log(`${operation} failed: ${error.message}`);
+
+    // 空の結果を返して、アプリを持続可能にする
+    return of(result as T);
+  };
+}
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      tap(heroes => this.log("fetched heroes")),
+      catchError(this.handleError<Hero[]>("getHeroes", []))
+    )
+  }
+
   getHero(id: number): Observable<Hero | undefined> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}}`
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
   }
 }
